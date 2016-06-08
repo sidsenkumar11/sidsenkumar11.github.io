a2fbf529ebf9d9aa481e37230a4fe7bcd2d7a000
Siddarth Senthilkumar <ssenthilkumar3@gatech.edu>
2016-06-08 09:47:20 +0200

    Finished Header and Footer
    TODO: Put Logo on left hand side of nav-bar or move Home link to left
    hand side

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,3 @@
 $(document).ready(function(){               
-  $('.materialboxed').materialbox();
-});
-$(document).ready(function(){
-  $('.materialboxed').materialbox();
+    $(".button-collapse").sideNav();
 });
